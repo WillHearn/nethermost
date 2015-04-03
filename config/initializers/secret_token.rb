8d9b89f1514879ce80250abbd165cc9d588f7778
William Hearn <william.hearn@sage.com>
2015-04-03 12:16:16 +0100

    Nethermost Start
    Start pull request

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nethermost::Application.config.secret_token = '1174cf675cb2b90ab659e09e824a7bd2a21929cd510202540b9acf9d6a274352fa7b38c843fccf90c778bc05637d81fa726badc4443883654b9ff96bf82a1af3'
